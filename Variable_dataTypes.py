e855f65e63cfc083156be77587e84166c79ec793
Muhammad Hataf <hatafmuhammad0@gmail.com>
2024-10-02 13:58:34 +0500

    python-Day-1

---
+++ Variable_dataTypes.py
@@ -0,0 +1,12 @@
+a = 123
+b = " hataf"
+print(a)
+print(b)
+#! print(a + b) # Getting error as same data type is not there 
+
+c = "Muhammad"
+d = None
+print(c + b)
+
+print(type(a))
+print(type(d))
