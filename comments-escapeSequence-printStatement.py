9db2f9050600c3a59bfed3d6297c24287400e999
Muhammad Hataf <hatafmuhammad0@gmail.com>
2024-10-02 13:44:23 +0500

    some message

---
+++ comments-escapeSequence-printStatement.py
@@ -0,0 +1,5 @@
+print("Hello\nthis is good ")
+
+# Escape Sequance : \n , 
+
+print("Hello How are you \"Good!\" \nThis is new Line ") #We use ===>  \"  <===== Double quote escape sequence character
---
+++ modulesAndPips.py
@@ -0,0 +1,5 @@
+# 2 Types of Module ===> These are piece of codes written already like random number generator like a library
+# 1) Built in Modules 
+# 2) External Modules 
+
+
