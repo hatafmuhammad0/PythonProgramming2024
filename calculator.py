d94562b81ac161d41b60b513f1fe83bb54399e79
Muhammad Hataf <hatafmuhammad0@gmail.com>
2024-10-02 14:51:19 +0500

    python day1 coding

---
+++ calculator.py
@@ -0,0 +1,20 @@
+""""
++ = Addition 
+- = Subtraction 
+* = Multiplication
+**= power
+/ = Division 
+//= Floor Division = > if result is 2.5 it will give 2 
+% = Modulus => This will give reminder 
+"""
+
+a =2 
+b =3
+
+print("Addition of a + b = " , a+b)
+print("Subtraction of b - a = " , b-a)
+print("Multiplication of a * b = " , a*b)
+print("Power of a ** b = " , a**b)
+print("b divided by  a = " ,b/a)
+print("Floor of b // a is ", b//a)
+print("Reminder of b %\"  a " , b%a)
---
+++ typecasting.py
@@ -0,0 +1,23 @@
+#What is Type casting ? 
+
+#example:
+
+a = "1"
+b = "2"
+print(a+b) #this is typecasting 
+
+#Python use a wide varity of functions or methods like: init() ,float() ,str(), ord() , hex() , 
+
+print(int(a) + int(b))
+
+#Two type of type Casting
+#1) Explicit : Done by me using method example is above
+#2) Implicit : Done by python
+
+#! Implicit type casting 
+x = 9.9 # data type is float 
+y = 1 #data type is integer
+
+print(x+y) #this data is implicitly change by python from inter to float
+
+
