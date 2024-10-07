993e7af6a17d8aa5db294c7af9e82b25ed2c596f
Muhammad Hataf <hatafmuhammad0@gmail.com>
2024-10-07 20:12:33 +0500

    Game

--- rock-paper-sessor-game.py
+++ rock-paper-sessor-game.py
@@ -6,15 +6,15 @@ def game(rounds):
     BotScore = 0
     Player_Score = 0
     totalGames = 0
-    computerTool = {
+    Weapons = {
         1 : "rock",
         2 : "paper",
         3 : "sessor"
     }
     while ((totalGames < 5) and (BotScore < 3) and (Player_Score <3)):
 
-        BotPlay = computerTool[random.randint(1,3)]
-        PlayerPlay = input("Player 2 : Select : Rock , Paper , Sessor : " ).lower()
+        BotPlay = Weapons[random.randint(1,3)]
+        PlayerPlay = Weapons[int(input("Choice number 1 = Rock , 2 = Paper , 3 = Sessor : "))]
         
         if BotPlay=="rock" and PlayerPlay == "sessor":
             BotScore +=1
