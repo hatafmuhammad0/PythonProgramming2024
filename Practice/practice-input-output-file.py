94a458442c53d0d8c77c60b00cd53c8baab5115c
Muhammad Hataf <hatafmuhammad0@gmail.com>
2024-10-08 22:20:03 +0500

    Day8

--- Practice/practice-input-output-file.py
+++ Practice/practice-input-output-file.py
@@ -1,13 +1,25 @@
-def genrateTable(n):
-    table = ""
 
-    for i in range (1,11):
-        table += f"{n} x {i} = {n*i} \n"
-    with open(f"table/table_{n}.txt","w") as f:
-        f.write(table)
+#! Write a table in different files
+# def genrateTable(n):
+#     table = ""
+
+#     for i in range (1,11):
+#         table += f"{n} x {i} = {n*i} \n"
+#     with open(f"table/table_{n}.txt","w") as f:
+#         f.write(table)
 
 
 
-for i in range(2,21):
+# for i in range(2,21):
     
-    genrateTable(i)
+#     genrateTable(i) 
+
+#! a file contain word "donkey" many times you need to replace it with "cow"
+
+def replaceWord(oldword,newWord,fileName):
+    with open(fileName) as f:
+        content = f.read()
+        with open (fileName,"w") as w:
+            w.write(content.replace(oldword,newWord))
+
+replaceWord("cow","lion","test1.txt")
