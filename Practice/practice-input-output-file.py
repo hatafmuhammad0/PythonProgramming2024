bc7054745f0af5e5838d52f4af0f36d8f65096b6
Muhammad Hataf <hatafmuhammad0@gmail.com>
2024-10-08 21:35:23 +0500

    Day8

---
+++ Practice/practice-input-output-file.py
@@ -0,0 +1,13 @@
+def genrateTable(n):
+    table = ""
+
+    for i in range (1,11):
+        table += f"{n} x {i} = {n*i} \n"
+    with open(f"table/table_{n}.txt","w") as f:
+        f.write(table)
+
+
+
+for i in range(2,21):
+    
+    genrateTable(i)
