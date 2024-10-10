f639d4d870a5c8e1c4af6f4834aee4e52b262fcc
Muhammad Hataf <hatafmuhammad0@gmail.com>
2024-10-10 21:26:17 +0500

    Day10

---
+++ Problem Solving/problems-loops.py
@@ -0,0 +1,35 @@
+
+#! counting positive numbers : example [1,2,-3,4,5,-6,7]
+
+# num = [1,2,-3,4,5,-6,7,-8,-9,-10]
+# count = 0
+# for i in num:
+#     if i>=0:
+#         count+=1
+
+# print(count)
+
+#! Sum of Even Numbers : 
+
+# num = [1,2,3,4,5,6,7,8,9,10]
+# countEvenNum = 0
+# for i in num:
+#     if i%2 ==0:
+#         countEvenNum +=1
+# print(countEvenNum)
+
+#! Multiplication table printer up to 10 but skip 5th iteration
+
+multiplicationNum = 3
+i =0
+while i<10:
+    i+=1
+    if i != 5:
+        print(f"{multiplicationNum} x {i} = {multiplicationNum*i}")
+    else:
+        None
+
+#! Reverse string 
+    
+
+
